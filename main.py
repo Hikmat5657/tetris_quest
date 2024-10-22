8627eef9f01f0a2252541670f9e3202448734c65
Hikmat5657 <hikmatsardarov923@gmail.com>
2024-10-22 20:12:25 +0400

    init pygame

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+frame_size_x = 400
+frame_size_y = 400
+window_screen = pygame.display.set_mode((frame_size_x, frame_size_y))
+pygame.display.set_caption("Tetris quest")
+clock = pygame.time.Clock()
+FPS = 60
+
