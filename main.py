f6e893630952272f8094a4333b83ada9a7171250
Hikmat5657 <hikmatsardarov923@gmail.com>
2024-10-29 19:27:25 +0400

    set windown size, create initial screen

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import pygame
 
 pygame.init()
 frame_size_x = 400
-frame_size_y = 400
+frame_size_y = 800
 window_screen = pygame.display.set_mode((frame_size_x, frame_size_y))
 pygame.display.set_caption("Tetris quest")
 clock = pygame.time.Clock()
 FPS = 60
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            pygame.quit()
+            exit()
+        
