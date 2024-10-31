ad17ea320f02ddc25d2df57a6165d1afffc7d1d5
Rifky Ariya <78594605+rif42@users.noreply.github.com>
2024-10-31 22:15:41 +0700

    show start screen

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ frame_size_y = 800
 window_screen = pygame.display.set_mode((frame_size_x, frame_size_y))
 pygame.display.set_caption("Tetris quest")
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 32)
 FPS = 60
+
+def start_screen():
+    game_message = font.render("space", False, "white")
+    game_message_rect = game_message.get_rect(
+        center=(frame_size_x//2, frame_size_y//2))
+    window_screen.blit(game_message, game_message_rect)
+
 
 while True:
     for event in pygame.event.get():
+        start_screen()
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             exit()
-        
+
+    pygame.display.update()
+    clock.tick(FPS)
