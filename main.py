db2313e37fc2c2d1a44223e8e991a22bdf8328b0
Hikmat5657 <hikmatsardarov923@gmail.com>
2024-11-21 19:49:11 +0400

    uniteing objects

--- main.py
+++ main.py
@@ -16,14 +16,12 @@ blocks = []
 last_spawn_time = 0  # Track the last time a block was spawned
 spawn_interval = 5000  # Interval in milliseconds (1000 ms = 1 second)
 
-
 def start_screen():
     game_message = font.render("press space to start", False, "white")
     game_message_rect = game_message.get_rect(
         center=(frame_size_x//2, frame_size_y//1.5))
     if game_init == False:
         window_screen.blit(game_message, game_message_rect)
-
 
 def game_active():
     if game_init == True:
@@ -44,19 +42,14 @@ def spawn_blocks():
     if randnum == 1:
         block = pygame.Rect(30, 30, 60, 60)
         block_2 = pygame.Rect (60,30,60,60)
-        blocks.append(block)
-        blocks.append(block_2)
+        block_united1 = pygame.Rect.union(block,block_2,)
+        blocks.append(block_united1)
     else:
         block_3 = pygame.Rect(30,30,60,60)
         block_4 = pygame.Rect(30,60,60,60)
-        blocks.append(block_3)
-        blocks.append(block_4)
-    
-
-    
-
-
-
+        block_united2 = pygame.Rect.union(block_3,block_4)
+        blocks.append(block_united2)
+            
 def block_gravity():
     for block in blocks:
         if block.bottom > 798:
@@ -64,12 +57,10 @@ def block_gravity():
         else:
             block.move_ip(0, 5)
         pygame.draw.rect(window_screen, color, block)
-        pygame.display.flip()
 
         # cleanup
         if block.midtop[1] > 800:
             blocks.remove(block)
-
 
 while True:
     for event in pygame.event.get():
