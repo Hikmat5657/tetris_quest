04b35f115aeae00d5373a2008192195505c6b73c
Hikmat5657 <hikmatsardarov923@gmail.com>
2025-01-28 19:58:19 +0400

    seperating blocks and renaming variables

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ game_init = False
 color = (0, 0, 255)
 font = pygame.font.SysFont(None, 32)
 FPS = 60
-blocks = []
-first_block = False  # variable acting as a state
+moving_block = []
+static_block = []
+should_spawn = True  # variable acting as a state
 last_spawn_time = 0  # Track the last time a block was spawned
 spawn_interval = 1000  # Interval in milliseconds (1000 ms = 1 second)
 
@@ -30,12 +31,12 @@ def game_active():
         block_gravity()
 
 def spawn_block_init():
-    global first_block
-    if first_block == False:
+    global should_spawn
+    if should_spawn == True:
         spawn_blocks()
-        first_block = True
+        should_spawn = False
 
-# spawn blocks only spawn once because once it spawns, first_block is set to true, then statement will not run anymore
+# spawn blocks only spawn once because once it spawns, should_spawn is set to true, then statement will not run anymore
 def spawn_blocks():
     randnum = random.randint(1, 2)
     randcoordinates = random.randint(1, 11) * 30
@@ -46,24 +47,25 @@ def spawn_blocks():
         block_2 = pygame.Rect(60, 30, 30, 30)
         block_united1 = pygame.Rect.union(block, block_2,)
         block_united1.center = (randcoordinates, 30)
-        blocks.append(block_united1)
+        moving_block.append(block_united1)
 
     else:
         block_3 = pygame.Rect(30, 30, 30, 30)
         block_4 = pygame.Rect(30, 60, 30, 30)
         block_united2 = pygame.Rect.union(block_3, block_4)
         block_united2.center = (randcoordinates, 30)
-        blocks.append(block_united2)
+        moving_block.append(block_united2)
 
 def block_gravity():
-    for i, block in enumerate(blocks):
+    global should_spawn
+    for i, block in enumerate(moving_block):
         # Check for collisions with other blocks
         should_move = True
         test_rect = block.copy()
         test_rect.move_ip(0, 5)  # Test the next position
 
         # Check collision with other blocks
-        for j, other_block in enumerate(blocks):
+        for j, other_block in enumerate(moving_block):
             if i != j and test_rect.colliderect(other_block):
                 should_move = False
                 break
@@ -71,12 +73,16 @@ def block_gravity():
         # Move the block if no collision and not at bottom
         if should_move and block.bottom < 798:
             block.move_ip(0, 5)
+        if block.bottom >= 799:
+            static_block.append(block)
+            moving_block.remove(block)
+            should_spawn = True 
 
         pygame.draw.rect(window_screen, color, block)
 
         # cleanup blocks that are off screen
         if block.midtop[1] > 800:
-            blocks.remove(block)
+            moving_block.remove(block)
 
 
 while True:
